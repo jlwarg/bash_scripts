5eca9ba55a859daa0e08eb9ef7916f838f22e92d
Jochen Luig <jochen.luig@gmail.com>
2016-03-05 23:39:16 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+My bash scripts
+===============
+
+
+post_installation.sh
+--------------------
+
+This is a very early iteration of my first shot at automating my setup of an Archlinux installation after a manual base install.
+**Probably not useful at all for the time being.**
---
+++ post_installation.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+echo "Arch Linux post installation script"
+
+add_user() {
+    echo -n "User to add: "
+    read USERNAME
+    useradd -m -G wheel $USERNAME
+}
+
+additional_packages() {
+    echo -n "package list file: "
+    read PACKAGES_FILE
+    if [[ ! -f $PACKAGES_FILE ]]; then
+        echo "file $PACKAGES_FILE doesn't exist."
+        exit 1
+    else
+        echo "installing packages"
+        CONTENTS=$(cat $PACKAGES_FILE)
+        sudo pacman -S --needed $CONTENTS
+    fi
+}
+
+enable_services() {
+    # enable all services
+}
+
+install_dotfiles() {
+    echo "Making sure git and stow are present"
+    pacman -S --needed git stow
+    echo -n "Github username: "
+    read GITHUBNAME
+    echo -n "dotfile repository: "
+    read DOTFILES
+    su $USERNAME
+    cd /home/$USERNAME
+    git clone http://github.com/$GITHUBNAME/$DOTFILES
+    cd $DOTFILES
+    STOWLIST=$(ls)
+    for DOTDIR in $STOWLIST; do
+        stow $DOTDIR
+    done
+}
